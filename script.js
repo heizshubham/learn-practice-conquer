a5eb2a4de1d98b7f5cb9ebde04a36f86e4ef0196
Shubham Pandey <69081168+heizshubham@users.noreply.github.com>
2024-09-03 20:08:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// document.getElementById('teacherBtn').addEventListener('click', function() {
+//     alert('Teacher button clicked!');
+// });
+
+// document.getElementById('studentBtn').addEventListener('click', function() {
+//     alert('Student button clicked!');
+// });
